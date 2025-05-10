6814078ce3f15aaa100f8b78d20850e1bd8d39d6
Nguyễn Bùi Mẫn Nhi <nhinbm.dev@gmail.com>
2025-05-10 18:23:25 +0700

    build: setup project: install shadcn + sanity

---
+++ src/sanity/schemaTypes/productType.ts
@@ -0,0 +1,25 @@
+import { defineField, defineType } from "sanity";
+
+export const productType = defineType({
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      type: "string",
+      title: "Name",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+      title: "Description",
+    }),
+  ],
+  preview: {
+    select: {
+      title: "name",
+      subtitle: "description",
+    },
+  },
+});
